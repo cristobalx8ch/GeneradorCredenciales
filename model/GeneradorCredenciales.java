c0928bedeca364fe10f3b4cd6046f41222e5f9ff
cristobalx8ch <martinezcristobal2005@gmail.com>
2025-05-04 18:22:29 -0400

    Configurando el README

--- model/GeneradorCredenciales.java
+++ model/GeneradorCredenciales.java
@@ -4,18 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GeneradorCredenciales {
-    // instancia unica de las clases
     private static GeneradorCredenciales instancia;
-    // lista que alamacena las credenciales
     private List<Credencial> credenciales;
 
-    // constructor privado para evitar instanciacion directa
+
     private GeneradorCredenciales() {
         this.credenciales = new ArrayList<>();
     }
 
-    // metodo estatico que retorna la unica instancia de la clase
-    // si no existe la crea
     public static GeneradorCredenciales obtenerInstancia() {
         if (instancia == null) {
             instancia = new GeneradorCredenciales();
@@ -23,7 +19,6 @@ public class GeneradorCredenciales {
         return instancia;
     }
 
-    // agrega una nueva credencial a la lista se utiliza el metodo clonar
     public void agregarCredenciales(Credencial c) {
         credenciales.add(c.clonar());
     }
